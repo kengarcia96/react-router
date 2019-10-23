a379c257c0fb65fffb7c12187a6fc7580e50b7fd
John Kenneth Garcia <kengarcia.96@gmail.com>
2019-10-23 18:41:19 +0800

    Added CounterGroup and ToDo List

---
+++ src/components/counter-group/Counter.js
@@ -0,0 +1,35 @@
+import React from 'react';
+// import '../App.css';
+import './Counter.css';
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {count:0}
+  }
+
+
+increaseNumber = () => {
+    this.setState({count: this.state.count +1});
+    this.props.onCounterValueChange(1);
+}
+
+decreaseNumber = () => {
+    this.setState({count: this.state.count -1});
+    this.props.onCounterValueChange(-1);
+}
+
+
+  render(){
+    return (
+      <div className="Counter">
+          <button onClick={this.increaseNumber}>+</button>
+          <span>{this.state.count}</span>
+          <button onClick={this.decreaseNumber}>-</button>
+      </div>  
+    );
+  }
+  }
+
+
+export default Counter;
